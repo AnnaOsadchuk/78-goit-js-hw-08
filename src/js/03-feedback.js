577085eb10450aa6a0512e472035ff77a967e7ce
AnnaOsadchuk <119502034+AnnaOsadchuk@users.noreply.github.com>
2023-05-14 14:38:47 +0200

    Done homewor-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+var throttle = require('lodash.throttle');
 
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
+const buttonEl = document.querySelector('button');
+
+const localState = localStorage.getItem('feedback-form-state');
+
+if (localState) {
+  const parsedLocalState = JSON.parse(localState);
+  emailEl.value = parsedLocalState.email;
+  messageEl.value = parsedLocalState.message;
+}
+
+formEl.addEventListener(
+  'input',
+  throttle(() => {
+    const emailValue = emailEl.value;
+    const messageValue = messageEl.value;
+    const inputArray = {
+      email: `${emailValue}`,
+      message: `${messageValue}`,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(inputArray));
+  }, 500)
+);
+
+buttonEl.addEventListener('click', event => {
+  event.preventDefault();
+  console.log(`email: ${emailEl.value}, message: ${messageEl.value}`);
+  localStorage.removeItem('feedback-form-state');
+  formEl.reset();
+});
