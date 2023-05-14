666e99ed3b263d1131d006f4c5b6b7e2c3af9703
AnnaOsadchuk <119502034+AnnaOsadchuk@users.noreply.github.com>
2023-05-14 14:42:42 +0200

    Add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onTimeupdate = function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+};
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
+);
