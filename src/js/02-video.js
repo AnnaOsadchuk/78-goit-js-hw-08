e3f488a3318f82156d7caba67072d00e9d16b296
AnnaOsadchuk <119502034+AnnaOsadchuk@users.noreply.github.com>
2023-05-17 21:37:41 +0200

    Fixed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,14 @@ var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onTimeupdate = function (data) {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+const onTimeupdate = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
 };
 
 player.on('timeupdate', throttle(onTimeupdate, 1000));
 
-player.setCurrentTime(
-  JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
-);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime != null) {
+  player.setCurrentTime(currentTime);
+}
